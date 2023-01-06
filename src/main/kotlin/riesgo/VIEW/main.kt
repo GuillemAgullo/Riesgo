94c93b568e5d8ac8def69fd0513f33c3fcf35b8a
GuillemAgullo <guillem.agullo@enti.cat>
2023-01-06 18:59:17 +0100

    error del mapa

--- src/main/kotlin/riesgo/VIEW/main.kt
+++ src/main/kotlin/riesgo/VIEW/main.kt
@@ -10,128 +10,163 @@ val Jugador2 = Jugador()
 //FUNCIÓ PER DEFINIR ELS TORNS DE LA FASE D'EXPLORACIÓ: SI ESCRIUS EL NOM D'UNA PROVÍNCIA, S'AFEGEIX UN SOLDAT A UNA PROVÍNCIA
 fun FaseExploracioTorns(Jugadorquelitoca: Jugador, Provinciaescollida: String){
     val Mapa = Mapa()
+
+    val Madrid = Mapa.provincies.find { it.nom == "Madrid" }
+    val Catalunya = Mapa.provincies.find { it.nom == "Catalunya" }
+    val Zaragoza = Mapa.provincies.find { it.nom == "Zaragoza" }
+    val Euskal_Herria = Mapa.provincies.find { it.nom == "Euskal_Herria" }
+    val Navarra = Mapa.provincies.find { it.nom == "Navarra" }
+    val La_Rioja = Mapa.provincies.find { it.nom == "La_Rioja" }
+    val Cantabria = Mapa.provincies.find { it.nom == "Cantábria" }
+    val Asturias = Mapa.provincies.find { it.nom == "Astúrias" }
+    val Galicia = Mapa.provincies.find { it.nom == "Galícia" }
+    val Castilla_Leon = Mapa.provincies.find { it.nom == "Castilla_León" }
+    val Valencia = Mapa.provincies.find { it.nom == "València" }
+    val Castilla_la_mancha = Mapa.provincies.find { it.nom == "Castilla_la_mancha" }
+    val Extremadura = Mapa.provincies.find { it.nom == "Extremadura" }
+    val Murcia = Mapa.provincies.find { it.nom == "Múrcia" }
+    val Andalucia = Mapa.provincies.find { it.nom == "Andalucía" }
+    val Islas_Canarias = Mapa.provincies.find { it.nom == "Islas_Canarias" }
+    val Ses_illes_balears = Mapa.provincies.find { it.nom == "Ses_illes_balears" }
     if (Provinciaescollida == "cataluña"){
-       ++Mapa.Cat.numSoldats
-        Mapa.Cat.propietari = Jugadorquelitoca
-        Mapa.Cat.color = Jugadorquelitoca.color
+        Catalunya?.numSoldats = Catalunya?.numSoldats?.plus(1)!!
+        Catalunya.propietari = Jugadorquelitoca
+        Catalunya.color = Jugadorquelitoca.color
     } else if (Provinciaescollida == "galicia"){
-        ++Mapa.Gal.numSoldats
-        Mapa.Gal.propietari = Jugadorquelitoca
-        Mapa.Gal.color = Jugadorquelitoca.color
+        Mapa.Galicia?.numSoldats = Mapa.Galicia?.numSoldats?.plus(1)!!
+        Mapa.Galicia.propietari = Jugadorquelitoca
+        Mapa.Galicia.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     } else if (Provinciaescollida == "Asturias"){
-        ++Mapa.Ast.numSoldats
-        Mapa.Ast.propietari = Jugadorquelitoca
-        Mapa.Ast.color = Jugadorquelitoca.color
+        Mapa.Asturias?.numSoldats = Mapa.Asturias?.numSoldats?.plus(1)!!
+        Mapa.Asturias.propietari = Jugadorquelitoca
+        Mapa.Asturias.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     } else if (Provinciaescollida == "cantabria"){
-        ++Mapa.Cnt.numSoldats
-        Mapa.Cnt.propietari = Jugadorquelitoca
-        Mapa.Cnt.color = Jugadorquelitoca.color
+        Mapa.Cantabria?.numSoldats = Mapa.Cantabria?.numSoldats?.plus(1)!!
+        Mapa.Cantabria.propietari = Jugadorquelitoca
+        Mapa.Cantabria.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "euskal herria"){
-        ++Mapa.Euk.numSoldats
-        Mapa.Euk.propietari = Jugadorquelitoca
-        Mapa.Euk.color = Jugadorquelitoca.color
+        Mapa.Euskal_Herria?.numSoldats = Mapa.Euskal_Herria?.numSoldats?.plus(1)!!
+        Mapa.Euskal_Herria.propietari = Jugadorquelitoca
+        Mapa.Euskal_Herria.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "navarra"){
-        ++Mapa.Nvr.numSoldats
-        Mapa.Nvr.propietari = Jugadorquelitoca
-        Mapa.Nvr.color = Jugadorquelitoca.color
+        Mapa.Euskal_Herria?.numSoldats = Mapa.Euskal_Herria?.numSoldats?.plus(1)!!
+        Mapa.Euskal_Herria.propietari = Jugadorquelitoca
+        Mapa.Euskal_Herria.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "la rioja"){
-        ++Mapa.Lrj.numSoldats
-        Mapa.Lrj.propietari = Jugadorquelitoca
-        Mapa.Lrj.color = Jugadorquelitoca.color
+        Mapa.La_Rioja?.numSoldats = Mapa.La_Rioja?.numSoldats?.plus(1)!!
+        Mapa.La_Rioja.propietari = Jugadorquelitoca
+        Mapa.La_Rioja.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "Zaragoza"){
-        ++Mapa.Zrg.numSoldats
-        Mapa.Zrg.propietari = Jugadorquelitoca
-        Mapa.Zrg.color = Jugadorquelitoca.color
+        Mapa.Zaragoza?.numSoldats = Mapa.Zaragoza?.numSoldats?.plus(1)!!
+        Mapa.Zaragoza.propietari = Jugadorquelitoca
+        Mapa.Zaragoza.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "castilla leon") {
-        ++Mapa.CsL.numSoldats
-        Mapa.CsL.propietari = Jugadorquelitoca
-        Mapa.CsL.color = Jugadorquelitoca.color
+        Mapa.Castilla_Leon?.numSoldats = Mapa.Castilla_Leon?.numSoldats?.plus(1)!!
+        Mapa.Castilla_Leon.propietari = Jugadorquelitoca
+        Mapa.Castilla_Leon.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "castlla la mancha") {
-        ++Mapa.CsM.numSoldats
-        Mapa.CsM.propietari = Jugadorquelitoca
-        Mapa.CsM.color = Jugadorquelitoca.color
+        Mapa.Castilla_la_mancha?.numSoldats = Mapa.Castilla_la_mancha?.numSoldats?.plus(1)!!
+        Mapa.Castilla_la_mancha.propietari = Jugadorquelitoca
+        Mapa.Castilla_la_mancha.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "madrid"){
-        ++Mapa.Mdz.numSoldats
-        Mapa.Mdz.propietari = Jugadorquelitoca
-        Mapa.Mdz.color = Jugadorquelitoca.color
+        Mapa.Madrid?.numSoldats = Mapa.Madrid?.numSoldats?.plus(1)!!
+        Mapa.Madrid.propietari = Jugadorquelitoca
+        Mapa.Madrid.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "valencia"){
-        ++Mapa.Vln.numSoldats
-        Mapa.Vln.propietari = Jugadorquelitoca
-        Mapa.Vln.color = Jugadorquelitoca.color
+        Mapa.Valencia?.numSoldats = Mapa.Valencia?.numSoldats?.plus(1)!!
+        Mapa.Valencia.propietari = Jugadorquelitoca
+        Mapa.Valencia.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "extremadura"){
-        ++Mapa.Ext.numSoldats
-        Mapa.Ext.propietari = Jugadorquelitoca
-        Mapa.Ext.color = Jugadorquelitoca.color
+        Mapa.Extremadura?.numSoldats = Mapa.Extremadura?.numSoldats?.plus(1)!!
+        Mapa.Extremadura.propietari = Jugadorquelitoca
+        Mapa.Extremadura.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "murcia"){
-        ++Mapa.Mur.numSoldats
-        Mapa.Mur.propietari = Jugadorquelitoca
-        Mapa.Mur.color = Jugadorquelitoca.color
+        Mapa.Murcia?.numSoldats = Mapa.Murcia?.numSoldats?.plus(1)!!
+        Mapa.Murcia.propietari = Jugadorquelitoca
+        Mapa.Murcia.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "andalucia"){
-        ++Mapa.And.numSoldats
-        Mapa.And.propietari = Jugadorquelitoca
-        Mapa.And.color = Jugadorquelitoca.color
+        Mapa.Andalucia?.numSoldats = Mapa.Andalucia?.numSoldats?.plus(1)!!
+        Mapa.Andalucia.propietari = Jugadorquelitoca
+        Mapa.Andalucia.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "islas baleares"){
-        ++Mapa.Ibl.numSoldats
-        Mapa.Ibl.propietari = Jugadorquelitoca
-        Mapa.Ibl.color = Jugadorquelitoca.color
+        Mapa.Ses_illes_balears?.numSoldats = Mapa.Ses_illes_balears?.numSoldats?.plus(1)!!
+        Mapa.Ses_illes_balears.propietari = Jugadorquelitoca
+        Mapa.Ses_illes_balears.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }else if (Provinciaescollida == "islas canarias"){
-        ++Mapa.Ica.numSoldats
-        Mapa.Ica.propietari = Jugadorquelitoca
-        Mapa.Ica.color = Jugadorquelitoca.color
+        Mapa.Islas_Canarias?.numSoldats = Mapa.Islas_Canarias?.numSoldats?.plus(1)!!
+        Mapa.Islas_Canarias.propietari = Jugadorquelitoca
+        Mapa.Islas_Canarias.color = Jugadorquelitoca.color
+        print(Mapa.asciimap)
     }
 
-    if (Mapa.Cat.numSoldats > 1){
-        Mapa.Cat.numSoldats = 1
+    if (Mapa.Catalunya!!.numSoldats > 1){
+        Mapa.Catalunya.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Gal.numSoldats > 1){
-        Mapa.Gal.numSoldats = 1
+    }else if (Mapa.Galicia!!.numSoldats > 1){
+        Mapa.Galicia.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Ast.numSoldats > 1){
-        Mapa.Ast.numSoldats = 1
+    }else if (Mapa.Asturias!!.numSoldats > 1){
+        Mapa.Asturias.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Cnt.numSoldats > 1){
-        Mapa.Cnt.numSoldats = 1
+    }else if (Mapa.Cantabria!!.numSoldats > 1){
+        Mapa.Cantabria.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Euk.numSoldats > 1){
-        Mapa.Euk.numSoldats = 1
+    }else if (Mapa.Euskal_Herria!!.numSoldats > 1){
+        Mapa.Euskal_Herria.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Nvr.numSoldats > 1){
-        Mapa.Nvr.numSoldats = 1
+    }else if (Mapa.Navarra!!.numSoldats > 1){
+        Mapa.Navarra.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Lrj.numSoldats > 1){
-        Mapa.Lrj.numSoldats = 1
+    }else if (Mapa.La_Rioja!!.numSoldats > 1){
+        Mapa.La_Rioja.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Zrg.numSoldats > 1){
-        Mapa.Zrg.numSoldats = 1
+    }else if (Mapa.Zaragoza!!.numSoldats > 1){
+        Mapa.Zaragoza.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.CsL.numSoldats > 1){
-        Mapa.CsL.numSoldats = 1
+    }else if (Mapa.Castilla_Leon!!.numSoldats > 1){
+        Mapa.Castilla_Leon.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.CsM.numSoldats > 1){
-        Mapa.CsM.numSoldats = 1
+    }else if (Mapa.Castilla_la_mancha!!.numSoldats > 1){
+        Mapa.Castilla_la_mancha.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Mdz.numSoldats > 1){
-        Mapa.Mdz.numSoldats = 1
+    }else if (Mapa.Madrid!!.numSoldats > 1){
+        Mapa.Madrid.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Vln.numSoldats > 1){
-        Mapa.Vln.numSoldats = 1
+    }else if (Mapa.Valencia!!.numSoldats > 1){
+        Mapa.Valencia.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Ext.numSoldats > 1){
-        Mapa.Ext.numSoldats = 1
+    }else if (Mapa.Extremadura!!.numSoldats > 1){
+        Mapa.Extremadura.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Mur.numSoldats > 1){
-        Mapa.Mur.numSoldats = 1
+    }else if (Mapa.Murcia!!.numSoldats > 1){
+        Mapa.Murcia.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.And.numSoldats > 1){
-        Mapa.And.numSoldats = 1
+    }else if (Mapa.Andalucia!!.numSoldats > 1){
+        Mapa.Andalucia.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Ibl.numSoldats > 1){
-        Mapa.Ibl.numSoldats = 1
+    }else if (Mapa.Ses_illes_balears!!.numSoldats > 1){
+        Mapa.Ses_illes_balears.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
-    }else if (Mapa.Ica.numSoldats > 1){
-        Mapa.Ica.numSoldats = 1
+    }else if (Mapa.Islas_Canarias!!.numSoldats > 1){
+        Mapa.Islas_Canarias.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
     }
+
 }
 
 fun main() {
@@ -142,8 +177,8 @@ fun main() {
         print("\u001b[2J")
 
 
-            println("==========================================================================================")
-            println("=============BENVINGUT A \"RIESGO\", EL RISK IBÈRIC=======================================")
+            println("\n==========================================================================================")
+            println("=============BENVINGUT A \"RIESGO\", EL RISK IBÈRIC=========================================")
             println("==========================================================================================")
             println("=============MENÚ DEL JOC=================================================================")
             println("==========================================================================================")
@@ -180,8 +215,8 @@ fun main() {
             Thread.sleep(250)
         }
         println("En aquesta fase, haureu de posar un soldat a cada província per torns.\n")
-        println("Per reclamar un territori, haureu d'escriure")
-        Thread.sleep(3000)
+        println("Per reclamar un territori, haureu d'escriure el nom de la província tal i com està escrit al mapa.\n")
+        Thread.sleep(6000)
 
         val Mapadeljoc = Mapa()
         print(Mapadeljoc.asciimap)
@@ -192,13 +227,6 @@ fun main() {
             print(Mapadeljoc.asciimap)
             println("\nTorn de ${Jugador2.nom} :\n Quina província vols explorar?\n")
             FaseExploracioTorns(Jugador2, readln())
-            print(Mapadeljoc.asciimap)
         }
-
-
-        print("\u001b[2J")
-
     }
-
-
 }
