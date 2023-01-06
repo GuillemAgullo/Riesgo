582dc556e7e124dc3122f4f6d1986863ccddef09
GuillemAgullo <guillem.agullo@enti.cat>
2023-01-06 14:00:58 +0100

    versió fent els torns i tal

--- src/main/kotlin/riesgo/VIEW/main.kt
+++ src/main/kotlin/riesgo/VIEW/main.kt
@@ -1,45 +1,85 @@
 import riesgo.CONTROLLER.Jugador
+import riesgo.MODEL.Provincia
 import riesgo.VIEW.Mapa
 
 //Aquest arxiu és el main i és des d'on cridaré totes les clases de l'inici del joc fins que s'acabi
+//PRIMER CREEM LES VARIABLES PER ALS DOS JUGADORS
+val Jugador1 = Jugador()
+val Jugador2 = Jugador()
 
-fun FaseExploracioTorns(Provinciaescollida: String){
+//FUNCIÓ PER DEFINIR ELS TORNS DE LA FASE D'EXPLORACIÓ: SI ESCRIUS EL NOM D'UNA PROVÍNCIA, S'AFEGEIX UN SOLDAT A UNA PROVÍNCIA
+fun FaseExploracioTorns(Jugadorquelitoca: Jugador, Provinciaescollida: String){
     val Mapa = Mapa()
     if (Provinciaescollida == "cataluña"){
        ++Mapa.Cat.numSoldats
+        Mapa.Cat.propietari = Jugadorquelitoca
+        Mapa.Cat.color = Jugadorquelitoca.color
     } else if (Provinciaescollida == "galicia"){
         ++Mapa.Gal.numSoldats
+        Mapa.Gal.propietari = Jugadorquelitoca
+        Mapa.Gal.color = Jugadorquelitoca.color
     } else if (Provinciaescollida == "Asturias"){
         ++Mapa.Ast.numSoldats
+        Mapa.Ast.propietari = Jugadorquelitoca
+        Mapa.Ast.color = Jugadorquelitoca.color
     } else if (Provinciaescollida == "cantabria"){
         ++Mapa.Cnt.numSoldats
+        Mapa.Cnt.propietari = Jugadorquelitoca
+        Mapa.Cnt.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "euskal herria"){
         ++Mapa.Euk.numSoldats
+        Mapa.Euk.propietari = Jugadorquelitoca
+        Mapa.Euk.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "navarra"){
         ++Mapa.Nvr.numSoldats
+        Mapa.Nvr.propietari = Jugadorquelitoca
+        Mapa.Nvr.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "la rioja"){
         ++Mapa.Lrj.numSoldats
+        Mapa.Lrj.propietari = Jugadorquelitoca
+        Mapa.Lrj.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "Zaragoza"){
         ++Mapa.Zrg.numSoldats
+        Mapa.Zrg.propietari = Jugadorquelitoca
+        Mapa.Zrg.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "castilla leon") {
         ++Mapa.CsL.numSoldats
+        Mapa.CsL.propietari = Jugadorquelitoca
+        Mapa.CsL.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "castlla la mancha") {
         ++Mapa.CsM.numSoldats
+        Mapa.CsM.propietari = Jugadorquelitoca
+        Mapa.CsM.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "madrid"){
         ++Mapa.Mdz.numSoldats
+        Mapa.Mdz.propietari = Jugadorquelitoca
+        Mapa.Mdz.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "valencia"){
         ++Mapa.Vln.numSoldats
+        Mapa.Vln.propietari = Jugadorquelitoca
+        Mapa.Vln.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "extremadura"){
         ++Mapa.Ext.numSoldats
+        Mapa.Ext.propietari = Jugadorquelitoca
+        Mapa.Ext.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "murcia"){
         ++Mapa.Mur.numSoldats
+        Mapa.Mur.propietari = Jugadorquelitoca
+        Mapa.Mur.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "andalucia"){
         ++Mapa.And.numSoldats
+        Mapa.And.propietari = Jugadorquelitoca
+        Mapa.And.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "islas baleares"){
         ++Mapa.Ibl.numSoldats
+        Mapa.Ibl.propietari = Jugadorquelitoca
+        Mapa.Ibl.color = Jugadorquelitoca.color
     }else if (Provinciaescollida == "islas canarias"){
         ++Mapa.Ica.numSoldats
+        Mapa.Ica.propietari = Jugadorquelitoca
+        Mapa.Ica.color = Jugadorquelitoca.color
     }
+
     if (Mapa.Cat.numSoldats > 1){
         Mapa.Cat.numSoldats = 1
         print("En la fase d'exploració només pots posar un soldat per zona")
@@ -109,7 +149,7 @@ fun main() {
             println("==========================================================================================")
 
         //CREEM UNA INSTÀNCIA DE LA CLASSE JUGADOR I LI DEMANEM AL USUARI QUE INTRODUEIXI EL NOM I EL COLOR AMB EL QUE VULGUI SER REPRESENTAT.
-        val Jugador1 = Jugador()
+
         println("Nom del jugador 1: ")
         Jugador1.nom = readLine()?:"Unnamed1"
         println("Color del jugador 1: ")
@@ -119,7 +159,7 @@ fun main() {
         Jugador1.numSoldats = 30
 
         //FEM LO MATEIX PERÒ PEL JUGADOR 2
-        val Jugador2 = Jugador()
+
         println("Nom del jugador 2: ")
         Jugador2.nom = readLine()?:"Unnamed2"
         println("Color del jugador 2: ")
@@ -140,13 +180,20 @@ fun main() {
             Thread.sleep(250)
         }
         println("En aquesta fase, haureu de posar un soldat a cada província per torns.\n")
+        println("Per reclamar un territori, haureu d'escriure")
         Thread.sleep(3000)
 
         val Mapadeljoc = Mapa()
         print(Mapadeljoc.asciimap)
 
-        println("Torn del jugador 1:\n Quina província vols explorar?")
-        FaseExploracioTorns(readln())
+        while (true){
+            println("\nTorn de ${Jugador1.nom} :\n Quina província vols explorar?\n")
+            FaseExploracioTorns(Jugador1, readln())
+            print(Mapadeljoc.asciimap)
+            println("\nTorn de ${Jugador2.nom} :\n Quina província vols explorar?\n")
+            FaseExploracioTorns(Jugador2, readln())
+            print(Mapadeljoc.asciimap)
+        }
 
 
         print("\u001b[2J")
